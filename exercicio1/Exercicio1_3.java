2c38981e00ac93f5a4d15293444ea3566bb93ed5
Mateus Santos <musiczfx@gmail.com>
2015-03-17 11:12:05 -0300

    Create Exercicio1_3.java
    Exercício 3.

---
+++ exercicio1/Exercicio1_3.java
@@ -0,0 +1,15 @@
+public class Exercicio1_3{
+	public static void main (String[] args) {
+		int a=45;
+		int b=20;
+		int c=3;
+    int soma=a+b+c;
+		int mult=a*b*c;
+		int div=(a/b)/c;
+		int sub=(a-b)-c;
+		System.out.println("A soma é: " + soma);
+		System.out.println("A multiplicação é: " + mult);
+		System.out.println("A divisão é: " + div);
+		System.out.println("A subtração é: " + sub);	
+	}
+}
