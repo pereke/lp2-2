8995b5311039fbb6862fd8a72b576e80b90069c9
Mateus Santos <musiczfx@gmail.com>
2015-03-17 11:12:52 -0300

    Create Exercicio1_5.java
    Exercício 5.

---
+++ exercicio1/Exercicio1_5.java
@@ -0,0 +1,15 @@
+public class Exercicio1_5{
+	public static void main (String[] args) {
+		double a=45;
+		double b=20;
+		double c=3;
+    double soma=a+b+c;
+		double mult=a*b*c;
+		double div=(a/b)/c;
+		double sub=(a-b)-c;
+		System.out.println("A soma é: " + soma);
+		System.out.println("A multiplicação é: " + mult);
+		System.out.println("A divisão é: " + div);
+		System.out.println("A subtração é: " + sub);	
+	}
+}
