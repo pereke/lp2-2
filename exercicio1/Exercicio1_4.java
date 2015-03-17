65046e5da8cef2c7c846c8b6e74e08cf36ac2964
Mateus Santos <musiczfx@gmail.com>
2015-03-17 11:12:29 -0300

    Create Exercicio1_4.java
    Exercício 4.

---
+++ exercicio1/Exercicio1_4.java
@@ -0,0 +1,15 @@
+public class Exercicio1_4{
+	public static void main (String[] args) {
+		float a=45;
+		float b=20;
+		float c=3;
+    float soma=a+b+c;
+		float mult=a*b*c;
+		float div=(a/b)/c;
+		float  sub=(a-b)-c;
+		System.out.println("A soma é: " + soma);
+		System.out.println("A multiplicação é: " + mult);
+		System.out.println("A divisão é: " + div);
+		System.out.println("A subtração é: " + sub);	
+	}
+}
